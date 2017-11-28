5efb803025343a9bd4e940e9d44f5cb329b72be7
offtechnologies <offtech@offtechnologies.github.io>
2017-11-28 10:57:50 +0000

    fix start script

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-if [ ! -f /etc/unbound/root.key ]; then
+if [ ! -f /usr/local/etc/unbound/root.key ]; then
   unbound-anchor
 fi
 
-chown -R $UID:$GID /var/unbound/ /etc/unbound /tmp
-
-exec /sbin/tini -- unbound -u $UID.$GID -P /tmp/unbound.pid -d
+exec /sbin/tini -- unbound -u $UID.$GID -d
