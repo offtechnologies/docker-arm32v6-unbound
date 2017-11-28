5065a5072bedb4e1520af13b0aa89fd226ef811a
offtechnologies <offtech@offtechnologies.github.io>
2017-11-28 16:13:17 +0000

    fix perms

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ if [ ! -f /usr/local/etc/unbound/root.key ]; then
   unbound-anchor
 fi
 
+chown -R unbound:unbound /usr/local/etc/unbound/
+
 exec /sbin/tini -- unbound -c /usr/local/etc/unbound/unbound.conf -d -v
